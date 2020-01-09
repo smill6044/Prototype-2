705c62d2e150fa103076e9c00ad44a905dedd8be
Cuellar-Miller <Cuel6044@skschools.org>
2020-01-09 11:49:17 -0800

    animals get destroyed when hit

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -15,11 +15,13 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // If an object goes past players view in game removes the object
         if (transform.position.z > topBound) 
         {
             Destroy(gameObject);
         } else if (transform.position.z < lowerBound) 
         {
+            Debug.Log("Game Over");
             Destroy(gameObject);
         }
     }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -3,25 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
-{
+{ 
     public GameObject[] animalPrefabs;
     private float spwanRangeX = 20;
     private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+      InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);  
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S)) 
-        {
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
+       
+    }
+
+    void SpawnRandomAnimal()
+    {
+         int animalIndex = Random.Range(0, animalPrefabs.Length);
             Vector3 spawnPos =new Vector3(Random.Range(-spwanRangeX, spwanRangeX), 0, spawnPosZ);
 
             Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
-        }
     }
 }
